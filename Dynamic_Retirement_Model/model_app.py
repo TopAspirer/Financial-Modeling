9a37fcfcaa4e2ff4917f411633ea8f59fbd56e88
Lloyd Tendo <153968328+TopAspirer@users.noreply.github.com>
2025-01-28 01:46:00 +0000

    The last commit didn't work... lets see if this works.

--- Dynamic_Retirement_Model/model_app.py
+++ Dynamic_Retirement_Model/model_app.py
@@ -6,9 +6,6 @@ from model_functions import salary_at_year, cash_saved_during_year, wealth_that_
 from dataclasses import dataclass
 
 
-st.write("Dynamic Retirement Model")
-st.write("by Lloyd Nsambu")
-
 @dataclass
 class ModelInputs:
     starting_salary: float = 0
@@ -45,7 +42,7 @@ There are more tools yet to be added.
 
 starting_salary = st.chat_input(
     "What is your starting/current salary",
-    input("70,000....?")
+    salary_salary = input("70,000....?")
 )
 data.starting_salary = float(starting_salary)
 
@@ -58,7 +55,7 @@ if starting_salary != float or int :
 
 promos_every_n_years = st.chat_input(
     "Enter every number of years you get a promotion",
-    input("5...?")
+    promos_every_n_years = input("5...?")
 )
 data.promos_every_n_years = promos_every_n_years
 
@@ -68,6 +65,6 @@ data.promos_every_n_years = promos_every_n_years
 
 promo_raise = st.chat_input(
     "What's raise you get for every promotion?",
-    input("Enter as a percentage")
+    promo_raise = input("Enter as a percentage")
 )
 data.promo_raise = promo_raise /100
