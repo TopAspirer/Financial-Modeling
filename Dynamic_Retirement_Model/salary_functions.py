a6f47bc7977e75bba2f041e71eccde51aa77fe23
Lloyd Tendo <153968328+TopAspirer@users.noreply.github.com>
2025-02-04 14:33:38 -0500

    Updated salary_functions.py
    just addes a test algo for a $100,000 salary

--- Dynamic_Retirement_Model/salary_functions.py
+++ Dynamic_Retirement_Model/salary_functions.py
@@ -51,7 +51,18 @@ class OntarioTaxRates:
     OT2: float = 105775
 
 
+# testing progressive algo
+salary = 105030
 
+if salary <= 105775:
+    b1 = ON_rate_1 * 52886
+    b2 = (salary - 52886 ) * ON_rate_2
+    
+    tax = b1 + b2 
+    print(f"{tax:,.2f}")
+
+else:
+    print(f"Can't calculate salary")
 
 
 
