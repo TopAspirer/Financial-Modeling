07dba01547b954dce10c0b3c8b2a05d19494cf73
Lloyd Tendo <tendolloyd@gmail.com>
2025-02-04 13:30:10 -0500

    Started working on the tax configuration in the salary_functions.py module.
    I think I'll have to create a class that's called the tax man to do the taxes over the user's lifetime.

--- Dynamic_Retirement_Model/salary_functions.py
+++ Dynamic_Retirement_Model/salary_functions.py
@@ -2,6 +2,59 @@
 ## This module will deal with everything regarding salary
 ## This module will house the the code for the json file that will hold the salary data. 
 
+from dataclasses import dataclass
+
+
+ 
+
+# Tax deductions
+SINGLE = 14000
+MARRIED_FILING_JOINTLY  = 29200
+HEAD_OF_HOUSE_HOLD = 21900
+
+# Need to create a data class that'll hold tax brackets
+
+@dataclass
+class FederalTaxRates:
+    """Holds 2025 Canadian Federal tax rates """
+
+    """On the portion of taxable $57,375 or less"""
+    _0_to_57375_: float = 0.15
+
+    """On the portion taxable income $52,375 to $114,750"""
+    _57375_to_114750: float = 0.205
+
+    """On the portion $114,750 to $117882"""
+    _114750_to_177882: float = 0.26
+
+    """On the portion $ 117,882 to $ 253,414"""
+    _177882_to_253414: float = 0.29
+
+    """On the portion $253,414"""
+    _253414: float = 0.33
+
+@dataclass
+class FederalDeductions:
+    SINGLE = 14000
+    MARRIED_FILING_JOINTLY  = 29200
+    HEAD_OF_HOUSE_HOLD = 21900
+
+
+@dataclass
+class OntarioTaxRates:
+    """Holds the 2025 Ontario Tax Rates"""
+
+    """On the portion $52,886 or less"""
+    OT1: float = 52886
+
+    """On the portion $52,886 to $105,775"""
+    OT2: float = 105775
+
+
+
+
+
+
 
 
 def salary_at_year (data, year):
@@ -17,5 +70,5 @@ def salary_at_year (data, year):
     return salary_t
 
 
-## Tax function to be added. 
+
 
